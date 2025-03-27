a844714136856049c3d82e7f88359334fb7dcfd3
Lucas Löffel <lucas@loeffel.io>
2025-03-27 13:07:36 +0100

    chore: bump downloads

--- .vuepress/components/FrontHeader.vue
+++ .vuepress/components/FrontHeader.vue
@@ -1,23 +1,50 @@
 <template>
-    <div id="front-header">
-        <img width="120" src="https://raw.githubusercontent.com/loeffel-io/ls-lint/master/assets/logo/ls-lint.png" alt="logo">
+  <div id="front-header">
+    <img
+      width="120"
+      src="https://raw.githubusercontent.com/loeffel-io/ls-lint/master/assets/logo/ls-lint.png"
+      alt="logo"
+    />
 
-        <h1>ls-lint</h1>
-        <h2>An extremely fast file and directory name linter</h2>
+    <h1>ls-lint</h1>
+    <h2>An extremely fast file and directory name linter</h2>
 
-        <p>
-            <a href="https://github.com/loeffel-io/ls-lint"><img alt="GitHub stars" src="https://img.shields.io/github/stars/loeffel-io/ls-lint?label=github%20stars"></a>
-            <a href="https://github.com/loeffel-io/ls-lint/actions/workflows/bazel.yml"><img src="https://github.com/loeffel-io/ls-lint/actions/workflows/bazel.yml/badge.svg?branch=master" alt="build"></a>
-            <!-- <a href="https://www.npmjs.com/package/@ls-lint/ls-lint"><img src="https://img.shields.io/npm/v/@ls-lint/ls-lint.svg?sanitize=true" alt="Version"></a> -->
-            <a href="https://www.npmjs.com/package/@ls-lint/ls-lint"><img src="https://img.shields.io/npm/dy/@ls-lint/ls-lint?label=npm%20downloads" alt="NPM Downloads"></a>
-            <a href="https://www.npmjs.com/package/@ls-lint/ls-lint"><img src="https://badgen.net/static/npm%20downloads%20total/6M+/green" alt="NPM Downloads"></a>
-            <a href="https://www.npmjs.com/package/@ls-lint/ls-lint"><img src="https://img.shields.io/npm/l/@ls-lint/ls-lint.svg?sanitize=true" alt="License"></a>
-        </p>
+    <p>
+      <a href="https://github.com/loeffel-io/ls-lint"
+        ><img
+          alt="GitHub stars"
+          src="https://img.shields.io/github/stars/loeffel-io/ls-lint?label=github%20stars"
+      /></a>
+      <a
+        href="https://github.com/loeffel-io/ls-lint/actions/workflows/bazel.yml"
+        ><img
+          src="https://github.com/loeffel-io/ls-lint/actions/workflows/bazel.yml/badge.svg?branch=master"
+          alt="build"
+      /></a>
+      <!-- <a href="https://www.npmjs.com/package/@ls-lint/ls-lint"><img src="https://img.shields.io/npm/v/@ls-lint/ls-lint.svg?sanitize=true" alt="Version"></a> -->
+      <a href="https://www.npmjs.com/package/@ls-lint/ls-lint"
+        ><img
+          src="https://img.shields.io/npm/dy/@ls-lint/ls-lint?label=npm%20downloads"
+          alt="NPM Downloads"
+      /></a>
+      <a href="https://www.npmjs.com/package/@ls-lint/ls-lint"
+        ><img
+          src="https://badgen.net/static/npm%20downloads%20total/7M+/green"
+          alt="NPM Downloads"
+      /></a>
+      <a href="https://www.npmjs.com/package/@ls-lint/ls-lint"
+        ><img
+          src="https://img.shields.io/npm/l/@ls-lint/ls-lint.svg?sanitize=true"
+          alt="License"
+      /></a>
+    </p>
 
-        <p id="get-started">
-            <a id="get-started-button" href="/2.3/getting-started/introduction.html">Get Started &rarr;</a>
-        </p>
-    </div>
+    <p id="get-started">
+      <a id="get-started-button" href="/2.3/getting-started/introduction.html"
+        >Get Started &rarr;</a
+      >
+    </p>
+  </div>
 </template>
 
 <script>
